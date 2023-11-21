1b78aa9cddb8defe764a208155bc808a3a9e9feb
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-11-21 18:08:16 +0100

    add inlay hints for rust analyzer

--- lua/piotreknow02/packer.lua
+++ lua/piotreknow02/packer.lua
@@ -44,6 +44,8 @@ return require('packer').startup(function(use)
             {'hrsh7th/nvim-cmp'},
             {'hrsh7th/cmp-nvim-lsp'},
             {'L3MON4D3/LuaSnip'},
+            -- Rust analyzer
+            {'simrat39/rust-tools.nvim'}
         }
     }
 
