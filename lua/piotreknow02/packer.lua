d095fa5c863df24fb98863c4b12ca600d6107d3c
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-12-07 13:33:36 +0100

    lsp additions, cellular autom, autotag and autopairs

--- lua/piotreknow02/packer.lua
+++ lua/piotreknow02/packer.lua
@@ -47,13 +47,30 @@ return require('packer').startup(function(use)
 
             -- LSP Support
             {'neovim/nvim-lspconfig'},
+
             -- Autocompletion
             {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
             {'hrsh7th/cmp-nvim-lsp'},
+
+            -- Snippets
             {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+
             -- Rust analyzer
             {'simrat39/rust-tools.nvim'}
         }
+    }
+
+    use 'eandrju/cellular-automaton.nvim'
+
+    use 'windwp/nvim-autopairs'
+
+    use {
+        'windwp/nvim-ts-autotag',
+        config = function() require('nvim-ts-autotag').setup() end
     }
 
     use 'lewis6991/gitsigns.nvim'
