cab7f5d0099dba6aa86764425c6518c2d546e18e
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2024-01-16 17:51:21 +0100

    telescope remap for hidden and gitignored files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,12 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fi", function ()
+    builtin.find_files{
+        hidden = true,
+        no_ignore = true,
+    }
+end, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
